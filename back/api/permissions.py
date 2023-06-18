df2586f17ae6cbc55afea5bfcf50138d65490d3d
sheverdin-mikhail <msheverdin648@gmail.com>
2023-06-19 01:04:36 +0400

    delete email verify cheking

--- back/api/permissions.py
+++ back/api/permissions.py
@@ -3,4 +3,5 @@ from rest_framework.permissions import BasePermission
 class IsAuthenticatedAndVerfyEmail(BasePermission):
    
     def has_permission(self, request, view):
-        return bool(request.user and request.user.is_authenticated and request.user.email_confirmed)
+        # return bool(request.user and request.user.is_authenticated and request.user.email_confirmed)
+        return bool(request.user and request.user.is_authenticated)
